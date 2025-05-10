e01946fd86deb04611518d1a9d019bd4627d829f
Никита Шатравский <shatick7975@gmail.com>
2025-05-10 16:41:44 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,122 @@
+const books = [
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Последнее желание',
+        publicationYear: 1993,
+        price: 650,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Меч Предназначения',
+        publicationYear: 1992,
+        price: 620,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Кровь эльфов',
+        publicationYear: 1994,
+        price: 700,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Час Презрения',
+        publicationYear: 1995,
+        price: 680,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Крещение огнём',
+        publicationYear: 1996,
+        price: 720,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Башня Ласточки',
+        publicationYear: 1997,
+        price: 750,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Ведьмак. Владычица Озера',
+        publicationYear: 1999,
+        price: 800,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и Тайная комната',
+        publicationYear: 1998,
+        price: 870,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и узник Азкабана',
+        publicationYear: 1999,
+        price: 890,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и Кубок огня',
+        publicationYear: 2000,
+        price: 920,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и Орден Феникса',
+        publicationYear: 2003,
+        price: 950,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и Принц-полукровка',
+        publicationYear: 2005,
+        price: 980,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и Дары Смерти',
+        publicationYear: 2007,
+        price: 1000,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Дж. К. Роулинг',
+        name: 'Гарри Поттер и Проклятое дитя',
+        publicationYear: 2016,
+        price: 1100,
+        bestseller: false,
+        inStock: true
+    }
+];
